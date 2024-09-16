9d8235a4a7db73a604b06f9630ba18db7c3a6610
pehringer <pehringer.jacob@gmail.com>
2024-09-15 21:20:38 -0600

    basic setup

---
+++ a_test.go
@@ -0,0 +1,11 @@
+package vectorize
+
+import (
+    "testing"
+    "fmt"
+)
+
+func TestHello(t *testing.T) {
+    fmt.Println("Hello, friend.")
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pehringer/vectorize
+
+go 1.23.1
