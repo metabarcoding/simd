953f50a116882f289b6308ce1c67f755839c2676
pehringer <pehringer.jacob@gmail.com>
2024-11-12 22:11:46 -0700

    arm64 float64 support

--- init_arm64.go
+++ init_arm64.go
@@ -10,6 +10,7 @@ import (
 func init() {
 	if neon.Supported() {
 		addFloat32 = neon.AddFloat32
+		addFloat64 = neon.AddFloat64
 		addInt32 = neon.AddInt32
 		addInt64 = neon.AddInt64
 		mulFloat32 = neon.MulFloat32
